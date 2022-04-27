058a1aecf787bce8416a18baae8c7fcbc6377dda
wxl2ld <1468258057@qq.com>
2022-04-27 18:41:41 +0800

    项目初始化结构创建

---
+++ beastmail-member/src/main/java/com/beast/beastmail/BeastmailMemberApplication.java
@@ -0,0 +1,13 @@
+package com.beast.beastmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeastmailMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeastmailMemberApplication.class, args);
+    }
+
+}
---
+++ beastmail-order/src/test/java/com/beast/beastmail/BeastmailOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beast.beastmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeastmailOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
